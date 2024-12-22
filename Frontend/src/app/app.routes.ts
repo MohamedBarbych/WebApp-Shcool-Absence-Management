97494abdee75ace5306b5d47f2e971619c523d6a
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-12-22 12:28:33 +0100

    Creating the angular front project and setup of routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { AbsencesComponent } from './components/absences/absences.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'students', component: StudentsComponent },
+  { path: 'absences', component: AbsencesComponent },
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
